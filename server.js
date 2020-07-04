cdfee15e6830b59cf80bddba5b06c50172ce34eb
Scott Glover <glover.scott92@gmail.com>
2020-07-04 10:57:53 -0700

    fixed sql firing issue, corrected some ORM and Controller syntax

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const routes = require("./controllers/burgers_controller");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -19,15 +21,16 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
 
 // ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// HANDLEBARS ENGINE
 // ================================================================================
 
-// require("./routes/apiRoutes")(app);
-// require("./routes/htmlRoutes")(app);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.use(routes);
 
 // =============================================================================
 // LISTENER
