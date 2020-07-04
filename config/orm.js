e4404e898f380536d3de607165dc10b11e98c4a9
Scott Glover <glover.scott92@gmail.com>
2020-07-04 13:21:41 -0700

    submit code to deploy to heroku

--- config/orm.js
+++ config/orm.js
@@ -30,65 +30,65 @@ function objToSql(ob) {
 
 // Object for all our SQL statement functions.
 const orm = {
-    selectAll: function (tableInput, cb) {
-        var queryString = "SELECT * FROM " + tableInput + ";";
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        });
-    },
-    insertOne: function (table, cols, vals, cb) {
-        var queryString = "INSERT INTO " + table;
+    : function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+        if (err) {
+            throw err;
+        }
+        cb(result);
+    });
+},
+insertOne: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
 
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-        console.log(queryString);
+    console.log(queryString);
 
-        connection.query(queryString, vals, function (err, result) {
-            if (err) {
-                throw err;
-            }
+    connection.query(queryString, vals, function (err, result) {
+        if (err) {
+            throw err;
+        }
 
-            cb(result);
-        });
-    },
-    updateOne: function (table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
+        cb(result);
+    });
+},
+updateOne: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
 
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-        console.log(queryString);
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+        if (err) {
+            throw err;
+        }
 
-            cb(result);
-        });
-    },
-    delete: function (table, condition, cb) {
-        var queryString = "DELETE FROM " + table;
-        queryString += " WHERE ";
-        queryString += condition;
+        cb(result);
+    });
+},
+delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
 
-        connection.query(queryString, function (err, result) {
-            if (err) {
-                throw err;
-            }
+    connection.query(queryString, function (err, result) {
+        if (err) {
+            throw err;
+        }
 
-            cb(result);
-        });
-    }
+        cb(result);
+    });
+}
 };
 
 // Export the orm object for the model.
